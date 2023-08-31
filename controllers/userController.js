f3d50faf947d9d770bc7ffa5da90da12003ea43e
ej <99721216+EjDadivas@users.noreply.github.com>
2023-08-31 15:07:00 +0800

    added updateAssets and other approaches

--- controllers/userController.js
+++ controllers/userController.js
@@ -70,28 +70,59 @@ const getAvailableAsset = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+// Approach 1: is to not make this function and simply uncomment the functions i created inside createTransaction and returnAsset.
+// Approaach 2: is to crerate a separate function such as provided below.
+// > But this can also be more inituitive if we set the parameters isCreate to options which will take up a string. It will receive the value "create" or "return" and the conditional statements will be based on the options.
+
+//Updating Asset
+const updateAsset = async (assetId, isCreate) => {
+  const asset = await Asset.findById(assetId);
+  if (!asset) {
+    return "Asset not found";
+  }
+
+  //if the action is for creating, we want to check if the availability is true or false.
+  if (isCreate && !asset.availability) {
+    return "Asset not available";
+  }
+
+  // const updatedAsset = await asset.updateOne({ availability: isReturn });
+  //if the action is create, we want to set availablity false. if return then we will tell that it is true.
+  asset.availability = isCreate ? false : true;
+  await asset.save();
+
+  return "Asset updated";
+};
 
 const createTransaction = async (req, res) => {
   try {
     const { assetId, dueAt } = req.body;
     const userId = req.user._id;
-    const asset = Asset.findById(assetId);
 
+    // Checking and Updating Asset
+
+    /*
+    const asset = await Asset.findById(assetId);
     if (!asset) {
       return res.status(404).json({ error: "Asset not found" });
     }
+
     if (!asset.availability) {
-      res.status(400).json({ error: "Asset not available" });
+      return res.status(400).json({ error: "Asset not available" });
     }
     const updatedAsset = await asset.updateOne({ availability: false });
     console.log(updatedAsset);
-
+    */
+    const updateResult = await updateAsset(assetId, true);
+    if (updateResult !== "Asset updated") {
+      return res.status(400).json({ error: updateResult });
+    }
+    // Creating new Transaction
     const transaction = new Transaction({
       userId,
       assetId,
       dueAt,
     });
-
     await transaction.save();
     res.status(201).json(transaction);
   } catch (error) {
@@ -102,11 +133,14 @@ const createTransaction = async (req, res) => {
 const getUserTransactions = async (req, res) => {
   try {
     const userId = req.user._id;
-    const assets = await Transaction.find({ user: userId })
-      .populate("user", "username")
-      .populate("asset", "name");
+    const transactions = await Transaction.find({ userId: userId })
+      .populate("userId", "username")
+      .populate("assetId");
 
-    res.status(200).json(assets);
+    if (transactions.length == 0) {
+      return res.status(404).json({ error: "No Transactions" });
+    }
+    res.status(200).json(transactions);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
   }
@@ -114,28 +148,45 @@ const getUserTransactions = async (req, res) => {
 
 const returnAsset = async (req, res) => {
   try {
-    const { id } = req.params;
+    const { id } = req.body;
     const userId = req.user._id;
 
+    // Checking Transaction
     const transaction = await Transaction.findById(id);
+
     if (!transaction) {
       return res.status(404).json({ error: "Transaction not found" });
     }
-    if (transaction.user !== userId) {
+
+    if (transaction.userId.toString() !== userId.toString()) {
       return res.status(403).json({ error: "Unauthorized" });
     }
-    const updatedTransaction = transaction.updateOne({ returnedAt: Date.now });
-    //updating asset
-    const asset = await Asset.findById(transaction.assetId);
+    if (transaction.returnedAt) {
+      return res.status(404).json({ error: "Transaction already completed" });
+    }
+
+    // Updating transation status and return date
+    // checks if the date today is later than due date
+    const status =
+      new Date() > transaction.dueAt ? "returned late" : "returned";
+    const updatedTransaction = await transaction.updateOne({
+      returnedAt: new Date(),
+      status: status,
+    });
+
+    //Updating asset availability to true
+    /*const asset = await Asset.findById(transaction.assetId);
 
     if (!asset) {
       return res.status(404).json({ error: "Asset not found" });
     }
-    if (!asset.availability) {
-      res.status(400).json({ error: "Asset not available" });
-    }
     const updatedAsset = await asset.updateOne({ availability: true });
-    console.log(updatedAsset);
+    console.log(updatedAsset); */
+
+    const updateResult = await updateAsset(transaction.assetId, false);
+    if (updateResult !== "Asset updated") {
+      return res.status(400).json({ error: updateResult });
+    }
 
     res.status(200).json(updatedTransaction);
   } catch (error) {
